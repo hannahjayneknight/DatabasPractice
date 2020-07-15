34c8e5fa1553f682903cc3eeb88f6f88cda79d6b
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-07-15 14:31:15 +0100

    15/07/20

--- testing/databasepractice2.js
+++ testing/databasepractice2.js
@@ -14,16 +14,16 @@ const db =
 // statement can be executed at a time
 db.serialize(function () {
     // Queries scheduled here will be serialized.
-    db.run("CREATE TABLE greetings(message text)")
+    db.run("CREATE TABLE greetings(message text)") // Create a new table.
       .run(`INSERT INTO greetings(message)
             VALUES('Hi'),
                   ('Hello'),
-                  ('Welcome')`)
+                  ('Welcome')`) // Insert data into the table.
       .each(`SELECT message FROM greetings`, function (err, row) {
         if (err){
           throw err;
         }
-        console.log(row.message);
+        console.log(row.message); // Query data from the table.
       });
 });
 
@@ -40,9 +40,46 @@ db.serialize(() => {
 
 */
 
+
+
+
+// parallelize means that queries are run in parallel but
+// should only be used if the database is very large
+db.parallelize(() => {
+    dbSum(1, 1, db);
+    dbSum(2, 2, db);
+    dbSum(3, 3, db);
+    dbSum(4, 4, db);
+    dbSum(5, 5, db);
+  });
+
+
 db.close(function (err) {
     if (err) {
         console.error(err.message);
     }
     console.log("Close the database connection.");
     });
+
+
+function dbSum(a, b, db) {
+    db.get('SELECT (? + ?) sum', [a, b], (err, row) => {
+        if (err) {
+        console.error(err.message);
+        }
+        console.log(`The sum of ${a} and ${b} is ${row.sum}`);
+    });
+    }
+
+/*
+HOW TO NEST THE PARALLELIZE METHOD
+
+db.parallelize(() => {
+  // queries will execute in parallel mode
+  db.parallelize(() => {
+    // queries will execute in parallel mode
+  });
+  // queries will execute in parallel mode
+});
+
+*/
