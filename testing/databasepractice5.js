031a8d99e5e3f2f48db7d34138f7725ad83e1692
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-07-16 13:41:58 +0100

    16/07/20

--- testing/databasepractice5.js
+++ testing/databasepractice5.js
@@ -10,75 +10,46 @@ const db =
     console.log("Connected to the sample database.");
 });
 
-// first you find the length of the database
-const queryLength = `SELECT count(*) AS rowCount FROM langs`;
+// finds a random word from the ones being tested
+// (depends on level - NEED TO CHANGE LEVEL FOR THIS)
+let level = 1;
+const queryWord = `SELECT * FROM (
+    SELECT * FROM langs ORDER BY langID LIMIT ${level}
+    ) ORDER BY RANDOM() LIMIT 1;`;
 
-
-var dbLength = [];
-db.serialize(() => {
-    db.get(queryLength, [], function (err, row) {
+const dbObj = {};
+db.serialize(function () {
+    db.get(queryWord, [], function (err, row) {
         if (err) {
             return console.error(err.message);
         }
-        dbLength.push(row);
-            // ? console.log("The length of the database is: " + dbLength)
-            // : console.log(`No length found`);
+        // adds the random to the database object
+        dbObj.word = row;
+
     });
 });
-
 
 
 db.close(function (err) {
     if (err) {
         console.error(err.message);
     }
-    
+
     console.log("Close the database connection.");
-    console.log(dbLength);
+    console.log(dbObj);
 });
 
-
 /*
-TRYING TO SAVE QUERY AS A VARIABLE
+FINDS THE LENGTH OF THE DATABASE
 
-async function f() {
-    var rowCount =
-    await db.get(queryLength, [], function (err, row) {
-    if (err) {
-        return console.error(err.message);
-    }
-    return row
-        ? console.log("The length of the database is: " + row.rowCount)
-        : console.log(`No length found`);
-    });
-    console.log(db);
-}
+// counts the number of rows in the database to find the length
+const queryLength = `SELECT count(*) AS rowCount FROM langs`;
 
-f();
-*/
-
-/*
-// then you can find a random word
-const langID = F.getRandomInt(0, obj.dbLength);
-const queryWord = `SELECT langID langID,
-name name
-FROM langs
-WHERE langID  = ?`;
-db.get(queryWord, [langID], function (err, row) {
-    if (err) {
-      return console.error(err.message);
-    }
-    return row
-      ? console.log(row.rowCount)
-      : console.log(`No language found with the id ${langID}`);
-});
-*/
-
-
-onrequest( () =>
-{
-    dblookup( () =>
-    {
-        sendresponsetoclient()
+db.get(queryLength, [], function (err, row) {
+        if (err) {
+            return console.error(err.message);
+        }
+        // adds the length of the database to the database object
+        dbObj.length = row.rowCount;
     })
-})
+*/
